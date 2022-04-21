d69e9d7ccfb1592f6c7f953180b38a4d91889fdd
Muhammad Rowaha <ashfaqrowaha@gmail.com>
2022-04-22 02:29:33 +0300

    added commands for a successful login

--- src/NetWorkProtocol/NetworkProtocol.java
+++ src/NetWorkProtocol/NetworkProtocol.java
@@ -1,6 +1,13 @@
 package NetWorkProtocol;
 
 public interface NetworkProtocol {
-    String User_LogOut = "/LOGOUT/";
+    String splitCharacter = "/";
+
+    String User_LogOut = "LOGOUT";
     String Invalid_LogOut = null;
+
+    String User_LogIn = "LOGIN"; //note that this will be extend by username and password 
+    //example command to log in a user LOGIN/MatErW3lon/uglyday@14
+
+    String SuccessFull_LOGIN = "SUCCESS";
 }
