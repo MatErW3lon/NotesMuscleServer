a6a140c3dd12688039cbf51b4e67454e85a5976c
Muhammad Rowaha <ashfaqrowaha@gmail.com>
2022-04-22 02:28:39 +0300

    added a method to create a query for logging in

--- src/MysqlQueries/SqlQueries.java
+++ src/MysqlQueries/SqlQueries.java
@@ -2,4 +2,8 @@ package MysqlQueries;
 
 public interface SqlQueries {
     final String getAllUsers = "select * from login_info";
+
+    static String createLoginUserQuery(String username, String password){
+        return "select BilkentID from login_info where UserName = '" + username + "' and PassWord = '" + password + "'";
+    }
 }
